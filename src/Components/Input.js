bff9319ef3de5fdc97e3017e7bfedf8b46636139
Ivijan-Antonijo Petras <iapetras@gmail.com>
2022-02-01 20:20:49 +0100

    input-medley

--- src/Components/Input.js
+++ src/Components/Input.js
@@ -1,11 +1,51 @@
 import React from "react";
 
+window.onload = function(){
+
+let container = document.querySelector('.card-container');
+let addBtn = document.querySelector('.addBtn');
+let inp = document.querySelector('.inp');
+
+function valueFetch(e){
+    const val = e.value;
+    return val;
+}
+
+function createCard(){
+    var vl = valueFetch(inp);
+    var newEl = document.createElement('div');
+    newEl.className = ".card";
+    newEl.innerText = vl;
+    return newEl;
+}
+
+function createNewNote(){
+    var nc = createCard();
+    
+    container.append(nc);
+}
+
+
+
+addBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    var card = document.createElement('div');
+    card.innerText = inp.value;
+    container.append(card);
+    inp = "";
+    
+});
+
+
+}
+
+
 function Input(){
     return(
-        <div className="input">
+        <div className="input"> 
             <form>
                 <label htmlFor="notes">Notes</label>
-                <input id="notes" type="text" name="notes" placeholder="Add new note" autoFocus/>
+                <input className="inp" id="notes" type="text" name="notes" placeholder="Add new note" autoFocus/>
                 <button type="submit" className="addBtn">Add Note</button>
             </form>
         </div>
